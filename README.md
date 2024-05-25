53164592408c371d8e0b4e2e50167e63803da3b4
masdimaseka <dimasekaid@gmail.com>
2024-05-25 21:38:31 +0800

    connect to database

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dims Blogging Website Exercise
+
+Video Reference
+https://youtu.be/Kkht2mwSL_I?si=175dGZgTFJ7q3Ffy
--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // untuk mengamankan link mongo
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;
